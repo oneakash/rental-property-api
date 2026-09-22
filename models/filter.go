04a2317034423fb8e85d091f425b1d4eefeb770b
oneakash <akashtazbirhossain@gmail.com>
2026-09-22 18:34:26 +0600

    add model for filtering

---
+++ models/filter.go
@@ -0,0 +1,14 @@
+package models
+type PropertyFilter struct{
+	MinPrice float64
+	MaxPrice float64
+	MinStarRating int
+	MinReviewScore float64
+	MinReviews int
+	Published *bool
+	PropertyType string
+	Feed int
+	MinBedroom int
+	Amenities []string
+	Limit int
+}
