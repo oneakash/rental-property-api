9316d1a954478ace8ffb3fa1ea80e198c6f4c3f9
oneakash <akashtazbirhossain@gmail.com>
2026-09-22 13:36:45 +0600

    add transformproperty service

--- services/property.go
+++ services/property.go
@@ -26,3 +26,39 @@ func LoadProperties() error{
 	)
 	return nil
 }
+func TransformProperty(source models.SourceProperty,) models.PropertyResponse{
+	response := models.PropertyResponse{
+		ID: source.ID,
+		Feed: source.Feed,
+		Published: source.Published,
+		GeoInfo: models.GeoInfo{
+			City: source.City,
+			Country: source.Country,
+			CountryCode: source.CountryCode,
+			Name: source.Display,
+			LocationID: source.LocationID,
+			State: source.State,
+			StateAbbr: source.StateAbbr,
+		},
+		Property: models.PropertyInfo{
+			Amenities: source.Amenities,
+			Name: source.PropertyName,
+			Slug: source.PropertySlug,
+			PropertyType: source.PropertyType,
+			Price: source.USDPrice,
+			ReviewScore: source.ReviewScore,
+			StarRating: source.StarRating,
+			Counts: models.CountInfo{
+				Bathroom: source.BathroomCount,
+				Bedroom: source.BedroomCount,
+				Reviews: source.NumberOfReview,
+				Occupancy: source.Occupancy,
+			},
+			Image: models.ImageInfo{
+				Count: len(source.Images),
+				Images: source.Images,
+			},
+		},
+	}
+	return response
+}
